5ee7d4fa9fff264aec5cd38b40f1013710244054
Brice <brice.virassamy@gmail.com>
2019-04-11 16:38:54 +0400

    Ajout personnage

--- app/src/main/java/com/exemple/jeuandroid/gameView.java
+++ app/src/main/java/com/exemple/jeuandroid/gameView.java
@@ -4,15 +4,23 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class gameView extends View {
 
     private Bitmap perso;
-
+    private int sceneX;
+    private int sceneY;
 
     public gameView(Context context) {
         super(context);
+
+        //Dimensions fenêtre
+        DisplayMetrics ecran = new DisplayMetrics();
+
+        sceneX = getWidth();
+        sceneY = getHeight();
 
         perso = BitmapFactory.decodeResource(getResources(), R.drawable.marche1);
     }
@@ -21,7 +29,8 @@ public class gameView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawBitmap(perso, 0, 0, null);
+
+        canvas.drawBitmap(perso, 100, 100, null);
 
     }
 }
