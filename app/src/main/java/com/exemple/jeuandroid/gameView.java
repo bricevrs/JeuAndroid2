a0194805fdc3b5b1bbf1780708d5bb9dce5061a5
Brice <brice.virassamy@gmail.com>
2019-04-12 00:18:32 +0400

    Positionnement background, perso, score ok (sans adaptation)

--- app/src/main/java/com/exemple/jeuandroid/gameView.java
+++ app/src/main/java/com/exemple/jeuandroid/gameView.java
@@ -4,33 +4,38 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.util.DisplayMetrics;
 import android.view.View;
 
 public class gameView extends View {
 
     private Bitmap perso;
-    private int sceneX;
-    private int sceneY;
-
+    private Bitmap background;
+    private Paint score = new Paint();
     public gameView(Context context) {
         super(context);
 
-        //Dimensions fenêtre
-        DisplayMetrics ecran = new DisplayMetrics();
-
-        sceneX = getWidth();
-        sceneY = getHeight();
 
         perso = BitmapFactory.decodeResource(getResources(), R.drawable.marche1);
+
+        background = BitmapFactory.decodeResource(getResources(), R.drawable.forest);
+
+        score.setColor(Color.WHITE);
+        score.setTextSize(36);
+        score.setTypeface(Typeface.DEFAULT_BOLD);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.drawBitmap(background,0,0,null);
+        canvas.drawBitmap(perso, 50, 750, null);
+        canvas.drawText("Score : ",850,850,score);
 
-        canvas.drawBitmap(perso, 100, 100, null);
 
     }
 }
